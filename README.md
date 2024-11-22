2841ef23884f4210120425b3e1ac201ab0f95965
YRZ <501863548@qq.com>
2024-11-22 16:16:46 +0800

    Finished workshop2,props and state

---
+++ README.md
@@ -0,0 +1,22 @@
+# Workshop 2 Conclusion!
+
+## to start
+
+Run `npx webpack` to build your react code! Then open up `index.html` in a browser of your choice.
+
+## don't touch
+
+the following files students do not need to edit. feel free to read them if you would like.
+
+```
+client/dist/index.html
+client/src/index.js
+client/src/utilities.js
+server/validator.js
+.babelrc
+.npmrc
+.prettierrc
+package-lock.json
+webpack.config.js
+```
+# catbook-react-workshop2
---
+++ client/src/components/pages/Profile.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import CatHappiness from "../modules/CatHappiness.js";
+import "../../utilities.css";
+import "./Profile.css";
+
+const Profile = () => {
+  const [catHappiness, setCatHappiness] = useState(0);
+
+  const incrementCatHappiness = () => {
+    setCatHappiness(catHappiness + 1);
+  };
+
+  return (
+    <div>
+      <div className="Profile-avatarContainer" onClick={incrementCatHappiness}>
+        <div className="Profile-avatar" />
+      </div>
+      <h1 className="Profile-name u-textCenter">YOUR NAME HERE</h1>
+      <hr className="Profile-line" />
+      <div className="u-flex">
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">About Me</h4>
+          <div id="profile-description">
+            Extra Challenge: Modify catbook to show a personalized description here!
+          </div>
+        </div>
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">Cat Happiness</h4>
+          <CatHappiness catHappiness={catHappiness} />
+        </div>
+        <div className="Profile-subContainer u-textCenter">
+          <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
+          <div id="favorite-cat">corgi</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
